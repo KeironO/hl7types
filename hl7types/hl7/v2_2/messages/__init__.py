cdb466bc4c811ed5dde79fdbc687c1dd9f9964df
KeironO <keiron.oshea@gmail.com>
2026-05-31 20:38:36 +0100

    feat: lazy import fixes two electric boogaloo

--- hl7types/hl7/v2_2/messages/__init__.py
+++ hl7types/hl7/v2_2/messages/__init__.py
@@ -1,4 +1,6 @@
 import importlib
+import sys
+import types
 
 _all_ = {
     'ACK', 'ADR_A19', 'ADT_A01', 'ADT_A02', 'ADT_A03', 'ADT_A04', 'ADT_A05',
@@ -16,8 +18,20 @@ _all_ = {
 }  # type: ignore
 
 
-def __getattr__(name: str):  # type: ignore[misc]
-    if name not in _all_:
-        raise AttributeError(f'module {__name__!r} has no attribute {name!r}')
-    mod = importlib.import_module(f'.{name}', __name__)
-    return getattr(mod, name)
+class _LazyModule(types.ModuleType):
+    def __getattr__(self, name: str):  # type: ignore[misc]
+        if name not in _all_:
+            raise AttributeError(f'module {self.__name__!r} has no attribute {name!r}')
+        mod = importlib.import_module(f'.{name}', self.__name__)
+        cls = getattr(mod, name)
+        self.__dict__[name] = cls
+        return cls
+
+    def __setattr__(self, name: str, value) -> None:  # type: ignore[override]
+        if name in _all_ and isinstance(value, types.ModuleType):
+            self.__dict__[name] = getattr(value, name)
+        else:
+            self.__dict__[name] = value
+
+
+sys.modules[__name__].__class__ = _LazyModule
--- hl7types/hl7/v2_5_1/groups/__init__.py
+++ hl7types/hl7/v2_5_1/groups/__init__.py
@@ -1,4 +1,6 @@
 import importlib
+import sys
+import types
 
 _all_ = {
     'ADR_A19_INSURANCE', 'ADR_A19_PROCEDURE', 'ADR_A19_QUERY_RESPONSE',
@@ -245,8 +247,20 @@ _all_ = {
 }  # type: ignore
 
 
-def __getattr__(name: str):  # type: ignore[misc]
-    if name not in _all_:
-        raise AttributeError(f'module {__name__!r} has no attribute {name!r}')
-    mod = importlib.import_module(f'.{name}', __name__)
-    return getattr(mod, name)
+class _LazyModule(types.ModuleType):
+    def __getattr__(self, name: str):  # type: ignore[misc]
+        if name not in _all_:
+            raise AttributeError(f'module {self.__name__!r} has no attribute {name!r}')
+        mod = importlib.import_module(f'.{name}', self.__name__)
+        cls = getattr(mod, name)
+        self.__dict__[name] = cls
+        return cls
+
+    def __setattr__(self, name: str, value) -> None:  # type: ignore[override]
+        if name in _all_ and isinstance(value, types.ModuleType):
+            self.__dict__[name] = getattr(value, name)
+        else:
+            self.__dict__[name] = value
+
+
+sys.modules[__name__].__class__ = _LazyModule
