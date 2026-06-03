adaf346f4dcf622dae3ec7c2fbf7e7e3c7636f92
KeironO <keiron.oshea@gmail.com>
2026-06-03 22:41:06 +0100

    feat: parser half implemented

--- hl7types/profiles/parser/__init__.py
+++ hl7types/profiles/parser/__init__.py
@@ -1,7 +1,11 @@
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
-from hl7types.profiles.parser.constraints import ProfileConstraints
+from hl7types.profiles.parser.constraints import (
+    ProfileConstraints,
+    SegGroupConstraint,
+    SegmentConstraint,
+)
 
 
 def parse_tables(path: str | Path) -> dict[str, dict[str, set[str]]]:
@@ -20,4 +24,28 @@ def parse_tables(path: str | Path) -> dict[str, dict[str, set[str]]]:
 
 
 def parse_profile(path: str | Path) -> ProfileConstraints:
-    pass
+    root = ET.parse(path).getroot()
+    static_def = root.find("HL7v2xStaticDef")
+    if static_def is None:
+        raise ValueError(
+            f"Could not parse profile {path}. No HL7v2StaticDEf element found in profile"
+        )
+
+    meta = root.find("MetaData")
+
+    # Strip all of the metadata from the root, static def and meta.
+    hl7_version = root.get("HL7Version")
+    msg_type = static_def.get("MsgType")
+    event_type = static_def.get("EventType")
+    msg_struct_id = static_def.get("MsgStructId")
+    name = meta.get("Name") if meta else None
+
+    children: list[SegmentConstraint | SegGroupConstraint] = []
+
+    for child in static_def:
+        if child.tag == "Segment":
+            print("PARSE SEGMENT")
+        elif child.tag == "SegGroup":
+            print("PARSE SEGGROUP")
+
+    print(children, hl7_version, msg_type, msg_struct_id, name, event_type)
