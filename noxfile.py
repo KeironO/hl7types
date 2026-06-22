644d33d113738037be898a11b7acef329ca8fc6d
KeironO <keiron.oshea@gmail.com>
2026-06-22 20:55:30 +0100

    feat: updated noxfile to include 3.14

--- noxfile.py
+++ noxfile.py
@@ -3,13 +3,13 @@ import glob
 import nox
 
 
-@nox.session(python=["3.10", "3.11", "3.12", "3.13"])
+@nox.session(python=["3.10", "3.11", "3.12", "3.13", "3.14"])
 def tests(session):
     session.run("uv", "sync", "--group", "dev", "--active", external=True)
     session.run("uv", "run", "--active", "pytest")
 
 
-@nox.session(python=["3.10", "3.11", "3.12", "3.13"])
+@nox.session(python=["3.10", "3.11", "3.12", "3.13", "3.14"])
 def examples(session):
     session.run("uv", "sync", "--group", "dev", "--active", external=True)
     for script in sorted(glob.glob("examples/*.py")):
